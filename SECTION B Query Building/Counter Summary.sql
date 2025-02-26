e348e5b633779f247db4077f81223f8fab744a36
KamanHang <kamanhangshrong@gmail.com>
2025-02-26 11:12:55 +0545

    Counter Summary Query Changes with accurate result

--- SECTION B Query Building/Counter Summary.sql
+++ SECTION B Query Building/Counter Summary.sql
@@ -3,13 +3,13 @@
 -- including the total queue called, serving, served, and no-show. 
 -- Note:  Use data from "2024-02-14" for the output. 
 
-SELECT
-    counter_id,
-    COUNT(queue_id) AS total_queue_called,
-    called_date,
-    SUM(CASE WHEN call_status_id = 1 THEN 1 ELSE 0 END) AS total_served,
-    SUM(CASE WHEN call_status_id = 2 THEN 1 ELSE 0 END) AS total_non_show,
-    SUM(CASE WHEN call_status_id IS NULL THEN 1 ELSE 0 END) AS total_serving
-FROM calls
-WHERE called_date = '2024-02-14'
-GROUP BY counter_id;
+-- Correct Output:
+SELECT 
+counters.id AS counter_id,
+CASE WHEN calls.queue_id IS NULL THEN calls.queue_id ELSE COUNT(queue_id) END AS total_queued,
+SUM(CASE WHEN calls.call_status_id = 1 THEN 1 WHEN  calls.call_status_id IS NULL THEN calls.call_status_id ELSE 0 END) AS total_served,
+SUM(CASE WHEN calls.call_status_id = 2 THEN 1 WHEN  calls.call_status_id IS NULL THEN calls.call_status_id ELSE 0 END) AS total_no_show
+FROM counters
+LEFT JOIN calls 
+ON counters.id = calls.counter_id AND calls.called_date = '2024-02-14'
+GROUP BY counters.id;
--- SECTION B Query Building/Service Summary.sql
+++ SECTION B Query Building/Service Summary.sql
@@ -1,4 +1,3 @@
 -- Service Summary
 -- Task:  Write an SQL query to get a summary of services  for any given date, 
 -- including the total visitors, queued, called, serving, served, and no-show.
-
